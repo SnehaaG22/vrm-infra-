fd60b2903eb3f0074b19ccfb816c12c96a618fe4
SnehaaG22 <snehaagaikwad22@gmail.com>
2026-01-30 20:49:48 +0530

    Delete apps directory

--- apps/notifications/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Notification
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in Notification._meta.fields]
-    search_fields = [f.name for f in Notification._meta.fields]
